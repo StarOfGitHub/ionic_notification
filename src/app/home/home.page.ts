dc1e90a03d6eb4bd35fc748c45294a69cc64705c
StarOfGitHub <sparklingstar08888@gmail.com>
2023-11-13 20:11:40 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+   constructor(private http: HttpClient) {}
 
-  constructor() {}
-
+  sendPushNotification() {
+    this.http.get('http://localhost:3000').subscribe((response) => {
+      console.log(response);
+    });
+  }
+  title = 'app';
 }
